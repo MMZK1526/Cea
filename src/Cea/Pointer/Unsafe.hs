e5997a69bae8808a8386f1bc38ce95a116347a0a
mmzk1526 <yc4120@ic.ac.uk>
2023-06-24 21:19:05 +0100

    [mmzk] (refactor) Clear Cea.Pointer.Unsafe until I decided with what to put inside

--- src/Cea/Pointer/Unsafe.hs
+++ src/Cea/Pointer/Unsafe.hs
@@ -1,8 +1,1 @@
 module Cea.Pointer.Unsafe where
-
--- import           Control.Monad.IO.Class
--- import           Foreign.Ptr
--- import           Foreign.Storable (Storable)
-
--- import           Cea.Pointer hiding (cast, copy, new, peek, poke)
--- import qualified Cea.Pointer as Cea
